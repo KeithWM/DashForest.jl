622a2fb028c5486d4290eb3dd147853157fb9cd8
KeithWM <keith@myerscough.nl>
2024-07-20 23:56:56 +0200

    Made one table look at the the other table for dropdown values (individual trees from a list of species!)

--- src/Application.jl
+++ src/Application.jl
@@ -2,15 +2,21 @@ function create()
     app = dash()
     
     app.layout = html_div([
-        # html_div([
-        #     dcc_input(
-        #         id="adding-rows-name",
-        #         placeholder="Enter a column name...",
-        #         value="",
-        #         style=Dict("padding" =>  10)
-        #     ),
-        #     html_button("Add Column", id="adding-rows-button", n_clicks=0)
-        # ], style=Dict("height" =>  50)),
+        dash_datatable(
+            id="table-dropdown",
+            data=Dict.(pairs.(eachrow(df))),
+            columns=[Dict("name" => i, "id" => i, "presentation" => "dropdown") for i in names(df)],
+            editable=true,
+            dropdown=Dict(
+                "climate" => Dict(
+                    "options" => [
+                        Dict("label" => i, "value" => i)
+                        for i in unique(df[!, "climate"])
+                    ]
+                )
+            )
+        ),
+        html_div(id="table-dropdown-container"),
     
         dash_datatable(
             id="adding-rows-table",
@@ -21,7 +27,7 @@ function create()
                 "renamable" =>  true
             ) for i in 1:4],
             data=[
-                Dict("column-$i" =>  (j + (i-1)*5)-1 for i in 1:4)
+                Dict("column-$i" => string.(j + (i - 1) * 5) for i in 1:4)
                 for j in 1:5
             ],
             editable=true,
@@ -39,6 +45,25 @@ function create()
         State("adding-rows-table", "columns")
     )
     
+    callback!(app,
+        Output("table-dropdown", "dropdown"),
+        Input("adding-rows-table", "data"),
+        Input("adding-rows-table", "columns")
+    ) do rows, columns
+        try
+            return Dict(
+                "climate" => Dict(
+                    "options" => [
+                        Dict("label" => i, "value" => i)
+                        for i in getindex.(rows, "column-1")
+                    ]
+                )
+            )
+        catch
+            throw(PreventUpdate())
+        end
+    end
+
     callback!(app,
         Output("adding-rows-graph", "figure"),
         Input("adding-rows-table", "data"),
--- src/DashForest.jl
+++ src/DashForest.jl
@@ -5,6 +5,13 @@ using CSV
 using DataFrames 
 using JSON3
 
+df = DataFrame([
+        (climate = "Sunny", temperature = 13, city ="NYC"),
+        (climate = "Snowy", temperature = 43, city ="Montreal"),
+        (climate = "Sunny", temperature = 50, city ="Miami"),
+        (climate = "Rainy", temperature = 30, city ="NYC")
+])
+
 include("Application.jl")
 export create
 
